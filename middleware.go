b25b15fbe67074fb759b814c09ab3314814040de
paul <eclipsepaulra@gmail.com>
2017-01-05 17:37:23 +0200

    Important fix

--- middleware.go
+++ middleware.go
@@ -7,7 +7,7 @@ type RequiredFunc func(*fasthttp.RequestCtx) Error
 func NewErrorMiddleware(f RequiredFunc) fasthttp.RequestHandler {
 	return func(ctx *fasthttp.RequestCtx) {
 		err := f(ctx)
-		if err.error != Nil {
+		if err != Nil {
 			ctx.SetBodyString(err.Error())
 			ctx.SetStatusCode(err.code)
 		}
